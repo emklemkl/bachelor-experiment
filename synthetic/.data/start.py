24e57f46c4e70a50e8412e6ca80fcf463ffc87c8
emklemkl <emil94_karlsson@outlook.com>
2025-03-28 16:23:39 +0100

    updated  start script and log to include id

--- synthetic/.data/start.py
+++ synthetic/.data/start.py
@@ -2,8 +2,9 @@ import datetime
 log_file_path = "./.data/logs/time.txt"
 
 # using now() to get current time
-participant_name = input("\nEnter your name (or the alias you used when signing up): ")
+participant_name = input("\nEnter your student acronym (ex: emkl21): ")
+participant_id = input("\nEnter your provided ID: ")
 current_time = datetime.datetime.now()
-print(f"\n{participant_name} Started the assignment: {current_time}\n")
+print(f"\n{participant_name}(ID: {participant_id})Started the assignment: {current_time}\n")
 with open(log_file_path, "a") as f:
-    f.write(f"{participant_name}\nStarted the assignment: {current_time}\n")
+    f.write(f"{participant_name}\nID {participant_id}\nStarted the assignment: {current_time}\n")
