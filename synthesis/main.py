a8353057e9bea63eaaa692e5c672601bbe159251
emklemkl <emil94_karlsson@outlook.com>
2025-04-01 14:28:40 +0200

    updated main and cleared the log file

--- synthesis/main.py
+++ synthesis/main.py
@@ -1,37 +1,6 @@
 #!/usr/bin/env python3
 
-##############################################################################################################
-##############################################################################################################
-############################################## BEFORE YOU START ############################################## 
-##############################################################################################################
-##############################################################################################################
-
 """
-If you haven't already, please read the instructions in the README.md.
-
-EXACTLY THE SAME INSTRUCTIONS YOU SEE BELOW CAN BE FOUND IN THE README.md
-(so feel free to delete any comments in this file if you want to) 
-
-START TIMER: python3 ./.data/start.py
-- Enter your student acronym: eg. (emkl21)
-- Enter your provided ID: eg. (1)
-- The timer has started.
-
-Test code: python3 main.py
-
-Stop timer: python3 ./.data/finished.py
-
-"""
-
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-
-
-"""
-
 Task 2 (Write Program):
 Write a function `task2` that takes one string argument and counts the number of words 
 that have exactly four characters and end with an 'e'.
@@ -64,6 +33,9 @@ INSTRUCTIONS:
 - Submit your `main.py` file as per the given instructions.
 """
 
-def task2(s: str) -> int:
+example_test_string = "Once upon a time. I had a baseball."
+
+def task2(str: str) -> int:
     pass
-task2()
+
+task2(example_test_string)
